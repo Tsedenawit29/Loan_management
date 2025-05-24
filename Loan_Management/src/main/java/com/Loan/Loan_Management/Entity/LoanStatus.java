50ad24170fb24ee00e21915114b863cbc7049824
Tsedenya <emkedemealemtsedenya@gmail.com>
2025-05-24 09:20:34 +0300

    the loan officer can change the status of the loan

---
+++ Loan_Management/src/main/java/com/Loan/Loan_Management/Entity/LoanStatus.java
@@ -0,0 +1,9 @@
+package com.Loan.Loan_Management.Entity;
+
+public enum LoanStatus {
+    PENDING,
+    APPROVED,
+    REJECTED,
+    DISBURSED,
+    PAID      // Optional: if you track full repayment
+}
